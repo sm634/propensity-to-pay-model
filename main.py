97056a9564bd4738e52ab422787e56c5af635f10
samukhia <safal.mukhia@capgemini.com>
2022-11-27 19:34:23 +0000

    corner shop data split
    Signed-off-by: samukhia <safal.mukhia@capgemini.com>

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from preprocessing_layer.engineer_features import write_model_dataset
+from preprocessing_layer.data_split import create_split_datasets
 
 
 # Run preprocessing layer to write model inputs to model layer
 write_model_dataset()
+# split corner shop data into train and test.
+create_split_datasets()
--- modelling_layer/models.py
+++ modelling_layer/models.py
@@ -3,11 +3,17 @@ import pandas as pd
 import numpy as np
 import pdb
 
-data = pd.read_csv("model_inputs/cornershop_data.csv").set_index('session_id')
+# y = data.completed_transaction.to_numpy()
+# X = data.drop(labels=['completed_transaction'], axis=1).to_numpy()
 
-y = data.completed_transaction.to_numpy()
-X = data.drop(labels=['completed_transaction'], axis=1).to_numpy()
 
-# train model on data
-clf = LogisticRegression(random_state=42).fit(X, y)
+class LogisticCLF:
+
+    def __init__(self, X, y):
+        self.X = X
+        self.y = y
+
+    def train(self):
+        # train model on data
+        clf = LogisticRegression(random_state=42).fit(self.X, self.y)
 
