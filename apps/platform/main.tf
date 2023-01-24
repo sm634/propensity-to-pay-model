38b8a080824b094b1bc134d69db054ac7904fc28
TanejaAnkisetty <taneja.ankisetty@capgemini.com>
2023-01-24 19:05:21 +0000

    make a new instance to check terraform apply

--- apps/platform/main.tf
+++ apps/platform/main.tf
@@ -10,6 +10,6 @@ resource "google_notebooks_instance" "instance" {
   machine_type = "n1-standard-4"
   vm_image {
     project      =  var.project_id
-    image_family = "tf-ent-2-9-cu113-notebooks"
+    image_family = "tf-ent-2-11-cu113-notebooks"
   }
 }
