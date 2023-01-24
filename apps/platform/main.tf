e4e3d24aa009a01fe20c5058ac24675135d94490
TanejaAnkisetty <taneja.ankisetty@capgemini.com>
2023-01-24 20:04:37 +0000

    make a new instance to check terraform apply

--- apps/platform/main.tf
+++ apps/platform/main.tf
@@ -1,9 +1,22 @@
-resource "google_notebooks_instance" "instance" {
-  name = "test1-propensity-model-train-tf"
-  location = "europe-west2-a"
-  machine_type = "n1-standard-4"
+resource "google_project_service" "notebooks" {
+  provider           = google
+  service            = "notebooks.googleapis.com"
+  disable_on_destroy = false
+}
+
+resource "google_notebooks_instance" "basic_instance" {
+  project      = var.project_id
+  name         = "notebooks-instance-basic"
+  provider     = google
+  location     = "europe-west2-a"
+  machine_type = "e2-medium"
+
   vm_image {
-    project      =  var.project_id
-    image_family = "tf-ent-latest-cpu"
+    project      = "deeplearning-platform-release"
+    image_family = "tf-ent-2-9-cu113-notebooks"
   }
+
+  depends_on = [
+    google_project_service.notebooks
+  ]
 }
