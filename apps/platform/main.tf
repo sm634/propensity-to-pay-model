e8cfe7b05f83f4fe9210ba42828f3512f2bb6c34
TanejaAnkisetty <taneja.ankisetty@capgemini.com>
2023-01-24 14:36:11 +0000

    updated vertex ai instance resource in main.tf

--- apps/platform/main.tf
+++ apps/platform/main.tf
@@ -2,4 +2,8 @@ resource "google_notebooks_instance" "instance" {
   name = "test1-propensity-model-train"
   location = "europe-west2-a"
   machine_type = "n1-standard-4"
+  vm_image {
+    project      =  var.project_id
+    image_family = "common-cpu-notebooks-debian-10"
+  }
 }
